c67ce7d4f2977f2db51db0c072be5942107600cf
Erik Rasmussen <rasmussenerik@gmail.com>
2017-09-11 16:00:58 +0200

    exported DocCheck by default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from 'react'
 import DocCheck from './DocCheck'
 
-const container = document.createElement('div')
-document.body.appendChild(container)
-ReactDOM.render(<DocCheck />, container)
+export default DocCheck
